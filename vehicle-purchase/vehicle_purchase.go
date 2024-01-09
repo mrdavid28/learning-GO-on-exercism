9fcc34e92aaf352f9a2531bf5be64fb92d9b633e
mrdavid28 <61464406+mrdavid28@users.noreply.github.com>
2024-01-09 16:03:03 +0100

    Update vehicle_purchase.go

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -1,7 +1,4 @@
-// package purchase
-package main
-
-import "fmt"
+package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
@@ -11,6 +8,7 @@ func NeedsLicense(kind string) bool {
 		return false
 	}
 }
+
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
@@ -22,12 +20,6 @@ func ChooseVehicle(option1, option2 string) string {
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
-/*
-Now that you made a decision, you want to make sure you get a fair price at the dealership.
-Since you are interested in buying a used vehicle, the price depends on how old the vehicle is.
-For a rough estimate, assume if the vehicle is less than 3 years old, it costs 80% of the original price it had when it was brand new.
-If it is at least 10 years old, it costs 50%.
-If the vehicle is at least 3 years old but less than 10 years, it costs 70% of the original price.*/
 func CalculateResellPrice(originalPrice, age float64) float64 {
 	if age < 3 {
 		return 0.8 * originalPrice
@@ -36,10 +28,4 @@ func CalculateResellPrice(originalPrice, age float64) float64 {
 	} else {
 		return 0.7 * originalPrice
 	}
-}
-
-func main() {
-	/*var option1, option2 string = "Buggatti", "Ford"
-	fmt.Println(ChooseVehicle(option1, option2))*/
-	fmt.Println(CalculateResellPrice(72000, 5))
 }
